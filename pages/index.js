8b054913724617e01dce786e43440cafd4ba3082
anargiris <anargiriss@hotmail.com>
2021-07-02 17:39:38 +0300

    new

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,23 @@ export default function Home() {
           name="google-site-verification"
           content="dIm_zFMudjHjPr2VUdGnlljx1MWI2v5bq2gCDz5o50c"
         />
-
+        {/* Global Site Tag (gtag.js) - Google Analytics */}
+        <script
+          async
+          src={`https://www.googletagmanager.com/gtag/js?id=${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}`}
+        />
+        <script
+          dangerouslySetInnerHTML={{
+            __html: `
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', '${process.env.NEXT_PUBLIC_GOOGLE_ANALYTICS}', {
+              page_path: window.location.pathname,
+            });
+          `,
+          }}
+        />
         <meta name="robots" content="index, follow" />
         <link rel="canonical" href="https://www.dimitrakoudas.gr" />
         <meta
